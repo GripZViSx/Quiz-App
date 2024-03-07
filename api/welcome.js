3ab853275619d29619a91daab9ef5b2ca15f926a
GripZViSx <grozersniper38237@gmail.com>
2024-03-07 19:18:01 +0530

    Removed files

--- api/welcome.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports = (req, res) => {
-   const filePath = path.join(__dirname, '../public/views/welcome.html');
-   const fileContents = fs.readFileSync(filePath, 'utf8');
-   res.status(200).send(fileContents);
-};
